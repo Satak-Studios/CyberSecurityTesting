57b6729fb1504b80fc89a5a417fa02456a3ec79d
Satak <100216561+Satak-Studios@users.noreply.github.com>
2023-06-17 18:09:26 +0530

    ProjectYam - v0.1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/save', (req, res) => {
+  const data = req.body.data;
+
+  fs.appendFile('1.txt', data + '\n', (err) => {
+    if (err) {
+      console.log('Error saving user information:', err);
+      res.sendStatus(500);
+    } else {
+      console.log('User information saved successfully.');
+      res.sendStatus(200);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
